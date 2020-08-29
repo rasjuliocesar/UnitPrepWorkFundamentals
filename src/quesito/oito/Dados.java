d55b1736bdf61ea5f3fea4089abc2e5d7067606b
Julio Cesar <rasjuliocesar@gmail.com>
2020-08-29 12:19:09 -0300

    Quesito 8 - DadosImpl

---
+++ src/quesito/oito/Dados.java
@@ -0,0 +1,7 @@
+package quesito.oito;
+
+public interface Dados {
+
+	public void adicionar(Transporte t);
+	public void excluir(Transporte t);
+}
---
+++ src/quesito/oito/DadosImpl.java
@@ -0,0 +1,19 @@
+package quesito.oito;
+
+import java.util.ArrayList;
+
+public class DadosImpl implements Dados {
+
+	private ArrayList<Transporte> lista = new ArrayList<>();
+
+	@Override
+	public void adicionar(Transporte t) {
+		lista.add(t);
+	}
+
+	@Override
+	public void excluir(Transporte t) {
+		lista.remove(t);
+	}
+
+}
